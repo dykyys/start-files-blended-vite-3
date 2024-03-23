62b70ed35ed13f8dcf96b5fe54880a9050fe6b19
Musteks555 <artemmustek73@gmail.com>
2024-03-23 12:05:50 +0200

    add task-2

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,11 @@
+import { BeatLoader } from 'react-spinners';
+
+import css from './Loader.module.css';
+
 export const Loader = () => {
-  return <h2>Loader</h2>;
+  return (
+    <div className={css.backdrop}>
+      <BeatLoader color="#40bea5" />
+    </div>
+  );
 };
