5fbd4543dc203ffc2993e28530cbb95de5f716ea
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-05-05 12:08:43 +0300

    regions

--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,31 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, SearchForm, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service';
 
 export const SearchCountry = () => {
+  const [searchParams, setSearchParams] = useSearchParams('');
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const region = searchParams.get('region');
+    if (!region) {
+      return;
+    }
+    const fetchRegion = async () => {
+      const countries = await fetchByRegion(region);
+      setCountries(countries);
+    };
+    fetchRegion();
+  }, [searchParams]);
+  const onSubmit = region => {
+    setSearchParams({ region });
+  };
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm onSubmit={onSubmit} />
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
