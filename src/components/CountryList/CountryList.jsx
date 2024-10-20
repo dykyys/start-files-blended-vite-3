be1326d262f4cd65f0f54955885fbd09c7d6b9fc
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-10-20 14:47:28 +0300

    add Home

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,3 +1,18 @@
-export const CountryList = () => {
-  return <h2>CountryList</h2>;
+import { Grid, GridItem } from 'components';
+import { Link } from 'react-router-dom';
+
+export const CountryList = ({ countries }) => {
+  return (
+    <Grid>
+      {countries.map(country => {
+        return (
+          <GridItem key={country.id}>
+            <Link>
+              <img src={country.flag} alt={country.country} />
+            </Link>
+          </GridItem>
+        );
+      })}
+    </Grid>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await getCountries();
+        setCountries(data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {loading && <Loader />}
+        <CountryList countries={countries} />
+        {error && (
+          <Heading title="Something went wrong, try again later" bottom />
+        )}
       </Container>
     </Section>
   );
